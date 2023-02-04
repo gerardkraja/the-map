7ebb2845f5090f65f7de1293f68af286270ed45a
gerardkraja <gerardkraja@gmail.com>
2023-02-04 10:20:00 +0100

    added api queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import { Marker, Popup, MapContainer, TileLayer, useMap } from 'react-leaflet'
 import { SearchControl, OpenStreetMapProvider } from "./components/Search.tsx";
 
+//TODO: Make api key env var & add to gitignore
+//TODO: put the useeffect in separate hook
 function App() {
+const [events, setEvents] = useState(undefined)
 const prov = OpenStreetMapProvider();
     const GeoSearchControlElement = SearchControl;
+useEffect(()=>{
+async function fetchData(){
+const clientId = 'MzE2NTUzNDh8MTY3NDgyNDA2MS43MjgyNjM'
+const response = await fetch(`https://api.seatgeek.com/2/events?client_id=${clientId}&per_page=50`)
+const data = await response.json()
+setEvents(data.events)
+}
+fetchData()
+},[])
   return (
     <div className="App">
 <MapContainer
@@ -20,7 +32,6 @@ options={{position:'bottom'}}
           provider={prov}
           showMarker={false}
           showPopup={false}
-          maxMarkers={3}
           retainZoomLevel={false}
           animateZoom={true}
           autoClose={true}
@@ -32,10 +43,10 @@ options={{position:'bottom'}}
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
-
-        <Marker position={[49.8397, 24.0297]} />
-        <Marker position={[52.2297, 21.0122]} />
-        <Marker position={[51.5074, -0.0901]} />
+{events && 
+events.map(event=>{return <Marker position={[event.venue.location.lat, event.venue.location.lon]} />
+})
+}
 </MapContainer>
     </div>
   )
